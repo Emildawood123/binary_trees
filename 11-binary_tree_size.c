2fc20892decb7f71ea3039d101430d390518186d
Emil dawood <edawood997@gmail.com>
2023-10-31 15:11:11 +0200

    size of the tree

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include <stddef.h>
 #include "binary_trees.h"
+/**
+ * binary_tree_size - to declaration the size of tree
+ * @tree: tree
+ * Return: size of tree
+*/
 size_t binary_tree_size(const binary_tree_t *tree)
 {
 if (tree == NULL)
