ee41e6f4a9a922a54dd6a972ed897030e25b256c
Emil dawood <edawood997@gmail.com>
2023-10-31 19:44:06 +0200

    done

---
+++ 15-binary_tree_is_full.c
@@ -0,0 +1,35 @@
+
+#include "binary_trees.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * binary_tree_is_full - that to add new right item
+ * @tree: tree
+ * Return:new node
+*/
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+int f = 1;
+helper3(tree, &f);
+return (f);
+}
+/**
+ * helper3 - taht help to check it is full or no
+ * @tree: tree
+ * @flag: flag
+ * Return: void
+*/
+void helper3(const binary_tree_t *tree, int *flag)
+{
+if (!tree)
+{
+return;
+}
+if (((tree->right) && (!tree->left)) || ((!tree->right) && (tree->left)))
+{
+*flag = 0;
+return;
+}
+helper3(tree->left, flag);
+helper3(tree->right, flag);
+}
--- binary_trees.h
+++ binary_trees.h
@@ -36,4 +36,6 @@ size_t binary_tree_nodes(const binary_tree_t *tree);
 size_t helper2(const binary_tree_t *tree);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
+int binary_tree_is_full(const binary_tree_t *tree);
+void helper3(const binary_tree_t *tree, int *flag);
 #endif
