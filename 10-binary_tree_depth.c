be11a7a85b7fbcf899bf03b5f81465919b37398a
Emil dawood <edawood997@gmail.com>
2023-10-31 15:09:34 +0200

    10

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -1,9 +1,16 @@
 #include <stddef.h>
 #include "binary_trees.h"
-size_t binary_tree_depth(const binary_tree_t *tree){
+/**
+ * binary_tree_depth - function to declaration the depth of tree
+ * @tree: tree
+ * Return: depth
+*/
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
 size_t i = 0;
 const binary_tree_t *temp = tree;
-if (!tree){
+if (!tree)
+{
 return (0);
 }
 while (temp->parent)
