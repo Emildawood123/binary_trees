4c0bdee92dffa0a1da4662597262c9a6bff78298
Emil dawood <edawood997@gmail.com>
2023-10-31 14:52:48 +0200

    three of write pring

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -1,11 +1,19 @@
 #include "binary_trees.h"
 #include <stdio.h>
 #include <stdlib.h>
-void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int)){
-if (!tree){
+/**
+ * binary_tree_preorder - that function pre
+ * @tree: tree
+ * @func: fun
+ * Return: void
+*/
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+{
+if (!tree)
+{
 return;
 }
 func(tree->n);
-binary_tree_inorder(tree->left,func);
-binary_tree_inorder(tree->right,func);
+binary_tree_inorder(tree->left, func);
+binary_tree_inorder(tree->right, func);
 }
--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -1,11 +1,19 @@
 #include "binary_trees.h"
 #include <stdio.h>
 #include <stdlib.h>
-void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int)){
-if (!tree){
+/**
+ * binary_tree_postorder - that function pre
+ * @tree: tree
+ * @func: fun
+ * Return: void
+*/
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
+{
+if (!tree)
+{
 return;
 }
-binary_tree_inorder(tree->left,func);
-binary_tree_inorder(tree->right,func);
+binary_tree_inorder(tree->left, func);
+binary_tree_inorder(tree->right, func);
 func(tree->n);
 }
